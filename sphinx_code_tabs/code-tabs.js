04dc9124e70b3922af7138de3babe343beb2c83b
Thomas Gläßle <t_glaessle@gmx.de>
2021-11-27 23:15:05 +0100

    Fix bug that selects all tabs on click
    Introduced in previous release.

--- sphinx_code_tabs/code-tabs.js
+++ sphinx_code_tabs/code-tabs.js
@@ -1,12 +1,12 @@
-const sphinx_code_tabs_onclick = function(button) {
-	const select = button.parentNode;
+const sphinx_code_tabs_onclick = function(clicked) {
+	const select = clicked.parentNode;
 	const book = select.parentNode;
 	for (const button of select.children) {
-    button.classList.toggle('selected', Object.is(button, button));
+    button.classList.toggle('selected', Object.is(button, clicked));
 	}
   for (const page of book.children) {
     if (page.hasAttribute('data-id')) {
-      page.classList.toggle('hidden', page.dataset.id != button.dataset.id);
+      page.classList.toggle('hidden', page.dataset.id != clicked.dataset.id);
 		}
   }
 };
